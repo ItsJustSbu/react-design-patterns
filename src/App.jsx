6eaf3ce82b72f87f7f2c698142602fe89d2bd622
Sibusiso Mfana <2307294@students.wits.ac.za>
2023-04-09 14:44:28 +0200

    completed uncontrolled-onboarding-flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { ControlledModal } from "./ControlledModal";
-import { useState } from "react";
-function App() {
+import UncontrolledOnboardingFlow from "./UncontrolledOnbordingFlow";
 
-	const [shouldShowModal, setShouldShowModal] = useState(false);
+const StepOne = ({goToNext}) => (
+<>
+<h1>Step 1</h1>
+<button onClick={goToNext}>Next</button>
+</>
+)
+const StepTwo = ({goToNext}) => (
+	<>
+	<h1>Step 2</h1>
+	<button onClick={goToNext}>Next</button>
+	</>
+	)
+const StepThree = ({goToNext}) => (
+	<>
+	<h1>Step 3</h1>
+	<button onClick={goToNext}>Next</button>
+	</>
+	)
+const StepFour = ({goToNext}) => (
+	<>
+	<h1>Step 4</h1>
+	<button onClick={goToNext}>Next</button>
+	</>
+	)
+
+function App() {
 
 	return (
 		
 		<>
 
-			<ControlledModal shouldShow={shouldShowModal} onRequestClose={()=>setShouldShowModal(false)}>
-				<h1>Hello</h1>
-			</ControlledModal>
-			<button onClick={()=> setShouldShowModal(!shouldShowModal)}>
-				{shouldShowModal?'hide Modal':'show Modal'}
-			</button>
+		<UncontrolledOnboardingFlow>
+			<StepOne />
+			<StepTwo />
+			<StepThree />
+			<StepFour />
+		</UncontrolledOnboardingFlow>
+
 		</>
 	);
 }
