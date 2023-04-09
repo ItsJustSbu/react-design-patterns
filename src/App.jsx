b90d626bf2cc977c676744cab26d1ef5a637bcab
Sibusiso Mfana <2307294@students.wits.ac.za>
2023-04-09 20:44:04 +0200

    starting file for custom  hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import printProps from "./printProps";
-import { UserInfo } from "./UserInfo";
-import { UserInfoForm } from "./UserInfoForm";
 
-// const UserInfoWrapped = printProps(UserInfo);
-// const UserInfoWithLoader = withUser(UserInfo, '123');
 
 function App(){
 	return(
 		<>
-		<UserInfoForm />
 
 		</>
 
