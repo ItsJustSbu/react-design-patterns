d384acc2dc315fc8136a06848dd83cddbd71cf4b
Sibusiso Mfana <2307294@students.wits.ac.za>
2023-04-09 19:46:39 +0200

    updated starter files for HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,11 @@
-import ControlledOnboardingFlow from './ControlledOnboardingFlow';
-import { useState } from 'react';
 
-const StepOne = ({goToNext}) => (
-<>
-<h1>Step 1</h1>
-<button onClick={()=>goToNext({name:'Sibusiso'})}>Next</button>
-</>
-)
-const StepTwo = ({goToNext}) => (
-	<>
-	<h1>Step 2</h1>
-	<button onClick={()=>goToNext({age:22})}>Next</button>
-	</>
-	)
-const StepThree = ({goToNext}) => (
-	<>
-	<h1>Step 3</h1>
-	<button onClick={()=>goToNext({hairColor:'Black'})}>Next</button>
-	</>
-	)
-const StepFour = ({goToNext}) => (
-	<>
-	<h1>Step 4</h1>
-	<button onClick={()=>goToNext({hobby:'coding'})}>Next</button>
-	</>
-	)
 
-function App() {
-
-	const [onboardingData, setOnboardingData] = useState({});
-	const [currentIndex, setCurrentIndex] = useState(0);
-
-	const goToNext = stepData =>{
-        setOnboardingData({...onboardingData, stepData});
-		setCurrentIndex(currentIndex+1);
-	}
-
-	return (
-		
+function App(){
+	return(
 		<>
-
-		<ControlledOnboardingFlow currentIndex={currentIndex} 
-		onNext={goToNext}
-		>
-			<StepOne />
-			<StepTwo />
-			<StepThree />
-			<StepFour />
-		</ControlledOnboardingFlow>
-
+		<h1>Your code goes here</h1>
 		</>
-	);
+	)
 }
 
 export default App;
