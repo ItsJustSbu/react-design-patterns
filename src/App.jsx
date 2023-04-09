5a598ebad74212f060666d9280058d742cb4709d
Sibusiso Mfana <2307294@students.wits.ac.za>
2023-04-09 06:31:28 +0200

    uncontrolled forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
-import { UserInfo } from './UserInfo';
-import DataSource from './DataSource';
-import { ProductInfo } from './ProductInfo';
-import axios from 'axios';
+
 
 function App() {
 
 	return (
 		<>
-			<DataSource getDataFunction={
-				async () =>{
-					const response = await axios.get("/users/123");
-					return response.data;
-				}
-			} ResourceName={"user"}>
-				<UserInfo />
-			</DataSource>
 			
-			<DataSource getDataFunction={
-				async () =>{
-					const response = await axios.get("/products/9012");
-					return response.data;
-				}
-			} ResourceName={"product"}>
-				<ProductInfo />
-			</DataSource>
 		</>
 	);
 }
